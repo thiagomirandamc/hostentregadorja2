f5fefab960289dd81ffffe74daa2d9759bdd9684
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-24 17:54:59 -0300

    ajustesarquivos

--- Entrega/solicitaentrega.php
+++ Entrega/solicitaentrega.php
@@ -8,12 +8,12 @@ include('../conexao2.php');
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Solicitar Entrega</title>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-        <link rel="stylesheet" href="css/bulma.min.css" />
-        <link rel="stylesheet" type="text/css" href="../../css/login.css">
-        <script type="text/javascript" src="../../jquery.min.js"></script>
-        <script type="text/javascript" src="../../smoothscroll.js"></script>
-        <script type="text/javascript" src="../../popper.min.js"></script>
-        <link rel="stylesheet" href="../../css/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bulma.min.css" />
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+        <script type="text/javascript" src="../jquery.min.js"></script>
+        <script type="text/javascript" src="../smoothscroll.js"></script>
+        <script type="text/javascript" src="../popper.min.js"></script>
+        <link rel="stylesheet" href="../css/bootstrap-4.4.1-dist/css/bootstrap.min.css">
    
     
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
@@ -21,7 +21,7 @@ include('../conexao2.php');
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="../../personalizado.js"></script>
+  <script type="text/javascript" src="../personalizado.js"></script>
         
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -33,7 +33,7 @@ include('../conexao2.php');
     <body>
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
    <a class="navbar-brand" href="#">
-       <img src="../../img/logo-systemway-transp.png" width="112" height="28" alt="">
+       <img src="../img/logo-systemway-transp.png" width="112" height="28" alt="">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
