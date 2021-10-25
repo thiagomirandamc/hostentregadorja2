be4e7ca66822f3c32ede32745bcf8670a139e8c8
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-25 16:28:52 -0300

    correcaoestabelecimento

--- Entrega/solicitarentrega.php
+++ Entrega/solicitarentrega.php
@@ -31,7 +31,7 @@ $row = mysqli_fetch_assoc($resultado);
  $idcliente = $row['idcliente'];
 
 $status = "Aberta";
-$sql = "INSERT INTO entregas (status, idcliente, logradouro, idbairro, idestalecimento, referencia, dataehorapedida, valor)  VALUES ('$status', '$idcliente', '$logradouro', '$idbairro', $idestabelecimento, '$referencia', NOW(), '$valor')";
+$sql = "INSERT INTO entregas (status, idcliente, logradouro, idbairro, idestabelecimento, referencia, dataehorapedida, valor)  VALUES ('$status', '$idcliente', '$logradouro', '$idbairro', $idestabelecimento, '$referencia', NOW(), '$valor')";
 $resul = mysqli_query($conexao2, $sql);
                 
 
