b1b29cbee61de021bb53afb7be137c778a1d0ed4
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-25 10:16:16 -0300

    login

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ session_start();
                     unset($_SESSION['nao_autenticado']);
                     ?>
                     <div class="box">
-                        <form action="login.php" method="POST">
+                        <form action="/login/login.php" method="POST">
                             <div class="field">
                                 <div class="control">
                                     <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
