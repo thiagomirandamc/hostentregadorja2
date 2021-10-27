47dc31f6695ebfb1736424805fc88700a6eb6f80
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-26 21:44:26 -0300

    ajuste

---
+++ login/cadastraruser.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include('../conexao2.php');
+
+/*vincular os campos do formulario com o banco*/
+$nome = mysqli_real_escape_string($conexao2, trim($_POST['nome']));
+$usuario = mysqli_real_escape_string($conexao2, trim($_POST['usuario']));
+$senha = mysqli_real_escape_string($conexao2, trim(md5($_POST['senha'])));
+$nomepermissao = mysqli_real_escape_string($conexao2, trim($_POST['permissao']));
+$resuesta = "SELECT * FROM permissao WHERE cargo = '$nomepermissao'";
+$resuleta = mysqli_query($conexao2, $resuesta);
+$row_oporest = mysqli_fetch_assoc($resuleta);
+ $idpermissao = $row_oporest['idpermissao'];
+
+ $nomeusuario = $_SESSION['nome'];
+ 	$resuesta = "SELECT * FROM usuario WHERE nome = '$nomeusuario'";
+	$resuleta = mysqli_query($conexao2, $resuesta);
+	$row_oporest = mysqli_fetch_assoc($resuleta);
+ 	$idusuario = $row_oporest['usuario_id'];
+
+/* verificar se não já tem o usuario que quer cadastrar e bloquear*/
+$sql = "select count(*) as total from usuario where usuario = '$usuario'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
+
+/*se o banco retornar com 1 linha afetada, redireciona pro cliente fazer outro cadastro */
+if($row['total'] == 1) {
+	$_SESSION['usuario_existe'] = true;
+	header('Location: cadastrouser.php');
+	exit;
+}
+
+$sql = "INSERT INTO usuario (nome, usuario, senha, data_cadastro, permissao, idquemcadastrou) VALUES ('$nome', '$usuario', '$senha', NOW(), '$idpermissao', '$idusuario')";
+
+if($conexao2->query($sql) === TRUE) {
+	$_SESSION['status_cadastro'] = true;
+}
+
+$conexao->close();
+
+header('Location: cadastrouser.php');
+exit;
+?>
---
+++ login/cadastrouser.php
@@ -0,0 +1,94 @@
+<?php
+include("../conexao2.php");
+session_start();
+?>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro CRM</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
+
+<body>
+    <?php
+    $usuario = $_SESSION['nome'];
+    $cult = "SELECT * FROM usuario WHERE nome ='$usuario'";
+                $execult = mysqli_query($conexao2, $cult);
+              if ($execult) {
+                    while ($rspl = mysqli_fetch_assoc($execult)) { 
+             $permissao = $rspl['permissao']; } }    ?> 
+      <?php
+                    if ($permissao == '1' || $permissao == '2') { ?>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Cadastro de usuários</h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <?php
+                    if($_SESSION['status_cadastro']):
+                    ?>
+                    <div class="notification is-success">
+                      <p>Cadastro efetuado!</p>
+                      <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['status_cadastro'])
+                    ?>
+                    <?php
+                    if($_SESSION['usuario_existe']):
+                    ?>    
+                    <div class="notification is-info">
+                        <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['usuario_existe']);
+                    ?>
+                    <div class="box">
+                        <form action="cadastrar.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" type="text" class="input is-large" placeholder="Usuário">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <div class="field"> 
+                            <label>Função</label>
+                            <br>
+                           
+                                <div class="select is-info">
+                                    <select name="permissao">   
+                                        <option>Cooperado</option>
+                                        <option>Entregador</option>
+                                        
+                                        
+                                    </select>
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+                    <?php } ?>
+</body>
+
+</html>
