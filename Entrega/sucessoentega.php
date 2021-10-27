a9f1e8336722615c21228c1d9142d8e10f92aa5d
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-27 20:36:36 -0300

    ajuste

---
+++ Entrega/sucessoentega.php
@@ -0,0 +1,40 @@
+<?php
+include("../conexao2.php");
+session_start();
+?>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tela Inicial</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
+
+<body>
+ 
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                
+                  
+                <span class="icon-text icon is-large has-text-success">
+                <span class="icon">
+                    <i class="fas fa-check-square"></i>
+                </span>
+                <span>Success</span>
+                </span>  
+                                
+                     
+                
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
