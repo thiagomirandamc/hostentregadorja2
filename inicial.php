578855d4b4eddc08c5fbf6dd1785fe776162ea3c
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-28 15:53:24 -0300

    ok

--- inicial.php
+++ inicial.php
@@ -15,6 +15,40 @@ session_start();
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!--  <a class="navbar-brand" href="#">
+       <img src="img/logo-systemway-transp.png" width="112" height="28" alt="">
+  </a> --> 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+        
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Inicial
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="">Teste</a>
+          
+        </div>
+      </li>
+      
+      
+     
+      
+        
+     
+      <div class="container">
+      <li class="nav-item">
+      <a href="login/logout.php">Sair</a>
+      </li>
+      </div>
+    </ul>
+  </div>
+</nav>
  
     <section class="hero is-success is-fullheight">
         <div class="hero-body">
