e8351f3150fd956ef5927235a2dd714845134ce8
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-29 10:00:33 -0300

    ok

--- conexao2.php
+++ conexao2.php
@@ -2,7 +2,7 @@
 
 define('HOST', 'br238.hostgator.com.br');
 define('USUARIO', 'syste104_root');
-define('SENHA','12345');
-define('DB', 'syste104_devpedidosja');
+define('SENHA','Pericles123*');
+define('DB', 'syste104_benevidesentregadorja');
 
 $conexao2 = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
