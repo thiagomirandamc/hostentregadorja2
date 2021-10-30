a1811cb98978e556049eb9526115702b47c2f5fa
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-10-30 19:06:15 -0300

    ok

---
+++ Relatorios/entregasporestabelecimento.php
@@ -0,0 +1,154 @@
+<?php
+include('../conexao2.php');
+session_start();
+include ('../login/verifica_login.php');
+
+
+
+
+
+
+?>
+<html>
+    <head>    
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Entregas por estabelecimento</title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="../css/bulma.min.css" />
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+        <script type="text/javascript" src="../jquery.min.js"></script>
+        <script type="text/javascript" src="../smoothscroll.js"></script>
+        <script type="text/javascript" src="../popper.min.js"></script>
+        <link rel="stylesheet" href="../css/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+   
+    
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../personalizado.js"></script>
+        
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+    </head>
+
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!--  <a class="navbar-brand" href="#">
+       <img src="img/logo-systemway-transp.png" width="112" height="28" alt="">
+  </a> --> 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+        
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Inicial
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="">Teste</a>
+          
+        </div>
+      </li>
+      
+      <div class="container">
+      <li class="nav-item">
+      <a href="../login/logout.php">Sair</a>
+      </li>
+      </div>
+    </ul>
+  </div>
+</nav>
+        
+
+    <div class="hero is-info welcome is-small">
+                        <div class="hero-body">
+                            
+                            <div class="container">
+                                <h1 class="title">
+                                    Relatório: Entregas do dia por estabelecimento
+                                </h1>
+                              
+                            </div>
+                        </div> 
+        </div>      
+        
+        <br>
+        <br>
+
+        <div class="column is-4 is-offset-4">
+
+            <form action="aceiteentregador.php" method="GET">
+                <label class="label">Estabelecimento</label>
+                <div class="field has-addons">
+                    <div class="control">
+                        <input class="input is-info" type="search"  name="identrega"  list="identrega" >
+                        <datalist id="identrega"> 
+                            <option></option> 
+                        </datalist>
+                    </div>
+                    <label class="label">Dia</label>
+                    <div class="container">
+                    <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+                        
+                    <div class="control">
+                        <button type="submit" class="button is-link is-rounded">Consultar</button>
+                    </div>
+                </div> 
+            </form>
+              
+            
+           
+               
+            <?php
+            if (isset($_GET['identrega'])) {
+               
+                $sql13 = "select * FROM entregas WHERE identrega = '$_GET[identrega]'";
+                $execut3 = mysqli_query($conexao2, $sql13);
+                
+                    while ($rs3 = mysqli_fetch_assoc($execut3)) {
+                    $identrega = $rs3['identrega'];
+                   
+                            } }  ?>
+    </div>
+  </div>
+    
+</div>
+                        
+                        <br>
+                    
+                     
+                        </div>
+                  
+                  </div>
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+         <script src="../bootstrap.min.js" type="text/javascript"></script>
+         <script src="../bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+         <script src="../bootstrap-datetimepicker.pt-BR.js" type="text/javascript"></script>
+         <script type="text/javascript">
+			$('.data_formato').datetimepicker({
+				weekStart: 1,
+				todayBtn: 1,
+                autoclose: 1,
+                todayHighlight: 1,
+                startView: 2,
+                forceParse: 0,
+                showMeridian: 1,
+                language: "pt-BR",
+                // startDate: '+0d'
+			});
+		</script>    
+                
+            
+        
+    </body>
+</html>
