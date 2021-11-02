923589fbfe8ac17f2fc2ccd22d615dd1109f8a44
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-11-02 11:25:18 -0300

    resetsen

---
+++ login/resetarsen.php
@@ -0,0 +1,37 @@
+<?php
+$dir = '../';
+session_start();
+include('../conexao2.php');
+
+/*vincular os campos do formulario com o banco*/
+$cpfoucnpj = mysqli_real_escape_string($conexao2, trim($_POST['cpfoucnpj']));
+$senha = mysqli_real_escape_string($conexao2, trim(md5($_POST['senha'])));
+
+
+
+
+/* verificar se não já tem o usuario que quer cadastrar e bloquear*/
+$sql = "select count(*) as total from usuario where cpfoucnpj = '$cpfoucnpj'";
+$result = mysqli_query($conexao2, $sql);
+$row = mysqli_fetch_assoc($result);
+
+/*se o banco retornar com 1 linha afetada, redireciona ai faz a alteracao */
+if($row['total'] == 1) {
+
+	$sql20 = "UPDATE usuario  SET senha = '$cliticket' WHERE cpfoucnpj = '$cpfoucnpj'";
+
+$exec = mysqli_query($conexao2, $sql20);
+	
+}
+
+
+
+if($conexao2->query($exec) === TRUE) {
+	$_SESSION['status_cadastro'] = true;
+}
+
+$conexao2->close();
+
+header('Location: resetsen.php');
+exit;
+?>
---
+++ login/resetsen.php
@@ -0,0 +1,80 @@
+<?php
+include("../conexao2.php");
+session_start();
+?>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro CRM</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<?php include '../menu.php';?>
+    <?php
+    $usuario = $_SESSION['nome'];
+    $cult = "SELECT * FROM usuario WHERE nome ='$usuario'";
+                $execult = mysqli_query($conexao2, $cult);
+              if ($execult) {
+                    while ($rspl = mysqli_fetch_assoc($execult)) { 
+             $permissao = $rspl['permissao']; } }    ?> 
+      <?php
+                    if ($permissao == '1' || $permissao == '2') { ?>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    
+                    <h3 class="title has-text-grey"><a  target="_blank">Entregador já</a></h3>
+                    <?php
+                    if($_SESSION['status_cadastro']):
+                    ?>
+                    <div class="notification is-success">
+                      <p>Alteração efetuada!</p>
+                      <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
+                    </div>
+                    <?php
+                    endif;
+                   
+                    ?>
+                    <?php
+                   
+                   
+                    ?>
+                    <div class="box">
+                        <form action="cadastraruser.php" method="POST">
+                            
+                            <div class="field">
+                                <div class="control">
+                                    <input name="cpfoucnpj" type="text" class="input is-large" placeholder="CPF ou CNPJ">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <div class="field"> 
+                            
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Alterar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+                    <?php } ?>
+</body>
+
+</html>
