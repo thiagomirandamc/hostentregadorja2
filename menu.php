34a4299b8929b8dbb76d856fcb4944b8d9bd7647
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-11-01 21:16:59 -0300

    ok

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Entregador Já</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,7 +18,17 @@
     <form class="form-inline my-2 my-lg-0">
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <?php echo $usuario ?>
+        
+        <?php
+            $result_clioport1sj = "SELECT * FROM usuario WHERE nome = '$usuario'";
+            $resultado_oport1sj = mysqli_query($conexao2, $result_clioport1sj);
+            while ($row_nomeclioport1sj = mysqli_fetch_assoc($resultado_oport1sj)){
+            $linkfotoj = $row_nomeclioport1sj['linkfoto'];
+                                               } ?> 
+                   <figure class="image is-24x24">
+                  <img class="is-rounded" src="<?php echo $linkfotoj ?>">
+                  </figure>
+        <?php echo $usuario ?>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Conta</a>
