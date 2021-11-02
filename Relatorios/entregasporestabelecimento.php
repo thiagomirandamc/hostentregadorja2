7b1ad18e214da01d0fd46b0d07c2eb587844efd9
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-11-02 09:52:16 -0300

    ok

--- Relatorios/entregasporestabelecimento.php
+++ Relatorios/entregasporestabelecimento.php
@@ -40,6 +40,25 @@ $parapesq = filter_input(INPUT_GET, 'data2', FILTER_SANITIZE_STRING);
 <script type="text/javascript" src="../personalizado.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+         <script src="../bootstrap.min.js" type="text/javascript"></script>
+         <script src="../bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+         <script src="../bootstrap-datetimepicker.pt-BR.js" type="text/javascript"></script>
+         <script type="text/javascript">
+			$('.data_formato').datetimepicker({
+				weekStart: 1,
+				todayBtn: 1,
+                autoclose: 1,
+                todayHighlight: 1,
+                startView: 2,
+                forceParse: 0,
+                showMeridian: 1,
+                language: "pt-BR",
+                // startDate: '+0d'
+			});
+		</script>
 
 </head>
 
@@ -265,23 +284,7 @@ while ($row_nomeclioport1sj = mysqli_fetch_assoc($resultado_oport1sj)){
                         </div>
                   
                   </div>
-                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-         <script src="../bootstrap.min.js" type="text/javascript"></script>
-         <script src="../bootstrap-datetimepicker.min.js" type="text/javascript"></script>
-         <script src="../bootstrap-datetimepicker.pt-BR.js" type="text/javascript"></script>
-         <script type="text/javascript">
-			$('.data_formato').datetimepicker({
-				weekStart: 1,
-				todayBtn: 1,
-                autoclose: 1,
-                todayHighlight: 1,
-                startView: 2,
-                forceParse: 0,
-                showMeridian: 1,
-                language: "pt-BR",
-                // startDate: '+0d'
-			});
-		</script>
+                  
                 
             
         
