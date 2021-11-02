045ffc547e684b17fd581ea3fcee2ca42547bb04
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-11-02 14:07:28 -0300

    ok

--- login/resetsen.php
+++ login/resetsen.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cadastro CRM</title>
+    <title>Alterar senha</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="../css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="../css/login.css">
