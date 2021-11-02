75b25d83e8775889815f0c9204dbe7f4b29829f2
thiagomirandamc <36346330+thiagomirandamc@users.noreply.github.com>
2021-11-02 09:58:56 -0300

    ok

--- Relatorios/entregasporestabelecimento.php
+++ Relatorios/entregasporestabelecimento.php
@@ -27,10 +27,7 @@ $parapesq = filter_input(INPUT_GET, 'data2', FILTER_SANITIZE_STRING);
         
        
         <link href="../css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css">
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+         
         <script type="text/javascript" src="../personalizado.js"></script>
    
    
